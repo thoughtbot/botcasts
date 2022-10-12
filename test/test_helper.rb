dc7cc8100ac1b4650aea476bd56c0c10eae3addc
Sean Doyle <sean.p.doyle24@gmail.com>
2022-10-12 14:55:45 -0400

    Depend on `action_dispatch-testing-integration-capybara`
    Installs the [thoughtbot/action_dispatch-testing-integration-capybara][]
    gem so that our application's `ActionDispatch::IntegrationTest` coverage
    can utilize Capybara-declared selectors when making assertions about a
    response's HTML body, _including_ those declared by
    `ActionText::SystemTestHelper` and `CapybaraAccessibleSelectors`.
    
    [thoughtbot/action_dispatch-testing-integration-capybara]: https://github.com/thoughtbot/action_dispatch-testing-integration-capybara#installation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "action_text/system_test_helper"
 
 module ActiveSupport
   class TestCase
@@ -14,4 +15,8 @@ module ActiveSupport
 
     # Add more helper methods to be used by all tests here...
   end
+end
+
+class ActionDispatch::IntegrationTest
+  include CapybaraAccessibleSelectors::Session
 end
