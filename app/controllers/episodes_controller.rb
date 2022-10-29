d6613cca6666d878083ea5cba585050f7567dd75
Sean Doyle <sean.p.doyle24@gmail.com>
2022-10-29 18:01:17 -0400

    Paginate with `pagy`
    Integrate with the [pagy][] gem to paginate records.
    
    Add links to the `episodes#index` templates to load pages through
    `a[rel="prev"]` and `a[rel="next"]` elements.
    
    [pagy]: https://tailwindui.com/templates/transmit

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,7 +1,7 @@
 class EpisodesController < ApplicationController
   def index
     @podcast = Podcast.find(params[:podcast_id])
-    @episodes = @podcast.episodes.most_recent_first
+    @page, @episodes = pagy @podcast.episodes.most_recent_first
   end
 
   def show
