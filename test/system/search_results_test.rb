98a2d3d2ecf066175ab19a392ee7ff6c9bd4ff25
Steve Polito <stevepolitodesign@users.noreply.github.com>
2022-12-02 10:33:40 -0500

    Lesson 3: Infinite scroll
    This commit progressively enhances the episodes index by automatically
    scrolling to the next page of episodes by leveraging [Turbo Frames][]
    and [Stimulus][].
    
    We set `[data-turbo-action]` to `replace` in order to [_replace_][]
    visit history while scrolling. In other words, if the user were to click
    the back button, the browser would not navigate to the previous page,
    but instead would navigate to the previously visited page.
    
    [Turbo Frames]: https://turbo.hotwired.dev/handbook/frames
    [Stimulus]: https://stimulus.hotwired.dev
    [_replace_]: https://turbo.hotwired.dev/handbook/drive#application-visits
    
    Co-authored-by: Sean Doyle <sean.p.doyle24@gmail.com>

--- test/system/search_results_test.rb
+++ test/system/search_results_test.rb
@@ -27,4 +27,45 @@ class SearchResultsTest < ApplicationSystemTestCase
       assert_no_selector :article, "Episode One"
     end
   end
+
+  test "scrolling loads the next page of episodes" do
+    with_pagy_defaults items: 10 do
+      query = "A match"
+      podcast = create(:podcast)
+      top_episode, bottom_episode, * = 11.times.map { create(:episode, podcast:, title: "##{_1}: #{query}") }
+
+      visit podcast_search_results_path(podcast, query:)
+
+      within :main do
+        assert_no_selector :article, top_episode.title
+        assert_link "Load older episodes"
+
+        scroll_to find :article, bottom_episode.title
+
+        assert_no_link "Load older episodes"
+        assert_selector :article, top_episode.title
+      end
+    end
+  end
+
+  test "load the previous page of episodes from the link" do
+    with_pagy_defaults items: 10 do
+      query = "A match"
+      podcast = create(:podcast)
+      top_episode, bottom_episode, * = 11.times.map { create(:episode, podcast:, title: "##{_1}: #{query}") }
+
+      visit podcast_search_results_path(podcast, query:, page: 2)
+
+      within :main do
+        assert_link "Load newer episodes"
+        assert_selector :article, top_episode.title
+        assert_no_link "Load older episodes"
+
+        click_on "Load newer episodes"
+
+        assert_selector :article, bottom_episode.title
+        assert_no_selector :article, top_episode.title
+      end
+    end
+  end
 end
