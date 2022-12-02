811f7a8fce61e005379e3cbb917f4f52d2466bf7
Steve Polito <stevepolitodesign@users.noreply.github.com>
2022-12-02 10:33:40 -0500

    Introduce infinite scroll
    This commit progressively enhances the episodes index by automatically
    scrolling to the next page of episodes by leveraging [Turbo Frames][]
    and [Stimulus][].
    
    We set `[data-turbo-action]` to `replace` in order to [_replace_][]
    visit history while scrolling. In other words, if the user were to click
    the back button, the browser would not navigate to the previous page,
    but instead would navigate to the previously visited page.
    
    [Turbo Frames]: https://turbo.hotwired.dev/handbook/frames
    [Stimulus]: https://stimulus.hotwired.dev
    [_replace_]: https://turbo.hotwired.dev/handbook/drive#application-visits
    
    Co-authored-by: Sean Doyle <sean.p.doyle24@gmail.com>

---
+++ app/javascript/controllers/element_controller.js
@@ -0,0 +1,7 @@
+import ApplicationController from "controllers/application_controller"
+
+export default class extends ApplicationController {
+  replaceWithChildren() {
+    this.element.replaceWith(...this.element.children)
+  }
+}
